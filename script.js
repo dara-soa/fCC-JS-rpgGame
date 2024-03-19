daeb5c6b4b6f865221be88a5f37a28a47c7dbdd7
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2024-03-18 23:33:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,17 +37,17 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
   button1.onclick = goStore;
   button2.onclick = goCave;
   button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  text.innerText = "You are in the town square. You see a sign that says \"Store.\"";
 }
 
 function goTown() {
-  update(locations);
+  update(locations[0]);
 }
 
 function goStore() {
