ffa75357cbc7af47c4918350b510a4ffce0b19af
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2024-03-22 13:17:31 -0300

    # Added sellWeapon function

--- script.js
+++ script.js
@@ -107,7 +107,11 @@ function buyWeapon() {
 
 function sellWeapon() {
   if (inventory.length > 1) {
-
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
   }
 }
 
