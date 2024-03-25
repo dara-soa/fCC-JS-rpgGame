5ac8862f77dde3b6c53b7a0041e9c4247c1c2535
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2024-03-25 12:09:14 -0300

    # Created attack function

--- script.js
+++ script.js
@@ -159,14 +159,28 @@ function goFight() {
   monsterHealthText.innerText = monsterHealth;
 }
 
-
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeapon].power;
+  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
 }
 
 function dodge() {
+
+}
+
+function defeatMonster() {
+
+}
+
+function lose() {
 
 }
